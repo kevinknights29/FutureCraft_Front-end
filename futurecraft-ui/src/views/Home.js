bff035cd8a88c17c792e3fc31bd6ce8dab6170ea
Kevin Knights <kevinknights29@hotmail.com>
2023-11-10 00:22:15 -0500

    added Hero component to Home view

--- futurecraft-ui/src/views/Home.js
+++ futurecraft-ui/src/views/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import Header from '../components/Header/Header';
+import Hero from '../components/Hero/Hero'
 
 function Home() {
   return (
     <>
       <Header />
+      <Hero />
       {/* Rest of your app components */}
     </>
   );
