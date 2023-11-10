4347250eec843b3f8872b9989308115dd5355471
Kevin Knights <kevinknights29@hotmail.com>
2023-11-10 00:10:33 -0500

    added home and global styles

--- futurecraft-ui/src/App.js
+++ futurecraft-ui/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import Home from './views/Home'; // Import the Home component
+import GlobalStyles from './styles/globalStyles';
+import Home from './views/Home';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Home /> {/* Use the Home component */}
     </div>
   );
