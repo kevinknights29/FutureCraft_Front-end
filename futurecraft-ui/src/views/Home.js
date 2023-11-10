eb2640481bd66bdc1f332eb288e5da67d3ad4bb2
Kevin Knights <kevinknights29@hotmail.com>
2023-11-10 00:46:22 -0500

    added Footer component to Home view

--- futurecraft-ui/src/views/Home.js
+++ futurecraft-ui/src/views/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import Header from '../components/Header/Header';
 import Hero from '../components/Hero/Hero'
+import Footer from '../components/Footer/Footer';
 
 function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Footer />
       {/* Rest of your app components */}
     </>
   );
